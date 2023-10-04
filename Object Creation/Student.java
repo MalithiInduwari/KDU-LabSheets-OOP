559c5631eca88d72d04b82fabaa44bee953e7d25
RHMI Wijepala <140041027+MalithiInduwari@users.noreply.github.com>
2023-10-04 18:56:04 +0530

    Add files via upload

---
+++ Object Creation/Student.java
@@ -0,0 +1,14 @@
+public class Student {
+
+
+
+        String x = "Sam";
+        String y = "John";
+        
+        public static void main(String[] args){
+            Student myObj = new Student();
+            System.out.println(myObj.x);
+            System.out.println(myObj.y);
+}
+    }
+    
